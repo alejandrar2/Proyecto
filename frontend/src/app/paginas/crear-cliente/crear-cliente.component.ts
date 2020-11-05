d6e147e44627168a04edb456cec41548071dabc1
alejandrar2 <40775960+alejandrar2@users.noreply.github.com>
2020-11-04 19:47:35 -0600

    cambios

--- frontend/src/app/paginas/crear-cliente/crear-cliente.component.ts
+++ frontend/src/app/paginas/crear-cliente/crear-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-crear-cliente',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearClienteComponent implements OnInit {
 
-  constructor() { }
+cliente ={
+  nombre : '',
+  apellido :'',
+  correo : '',
+  contrasenia: ''
+}
+
+  constructor( private clienteService : ClienteService ) {
+   }
 
   ngOnInit(): void {
+  }
+  crearCliente(){
+
+    this.clienteService.añadircliente(this.cliente).subscribe((data:any)=>{
+     console.log(data);
+    } );
+   // console.log(this.cliente);
   }
 
 }
